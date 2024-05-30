f06dc9408f6b742458c3e7cb34ee458d2462047d
“Dino” <daringamaleldin@hotmail.com>
2024-05-30 19:42:54 +0000

    update

---
+++ app/recommender.py
@@ -0,0 +1,32 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics.pairwise import cosine_similarity
+import pandas as pd
+
+def fetch_song_features(song_ids):
+    client_id = app.config['SPOTIPY_CLIENT_ID']
+    client_secret = app.config['SPOTIPY_CLIENT_SECRET']
+
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id=client_id, client_secret=client_secret))
+
+    features = []
+    for song_id in song_ids:
+        features.append(sp.audio_features(song_id)[0])
+    
+    return features
+
+def preprocess_features(features):
+    df = pd.DataFrame(features)
+    numerical_features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
+    scaler = StandardScaler()
+    df[numerical_features] = scaler.fit_transform(df[numerical_features])
+    return df, numerical_features
+
+def recommend_songs(liked_song_id, features_df, numerical_features, top_n=10):
+    similarity_matrix = cosine_similarity(features_df[numerical_features])
+    idx = features_df.index[features_df['id'] == liked_song_id].tolist()[0]
+    sim_scores = list(enumerate(similarity_matrix[idx]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    top_songs = [features_df.iloc[i[0]]['id'] for i in sim_scores[1:top_n+1]]
+    return top_songs
---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from flask import request, jsonify, render_template
+from app import app
+from app.recommender.py import fetch_song_features, preprocess_features, recommend_songs
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/recommend', methods=['POST'])
+def recommend():
+    data = request.json
+    liked_song_id = data.get('liked_song_id')
+    song_ids = data.get('song_ids')
+    
+    features = fetch_song_features(song_ids)
+    features_df, numerical_features = preprocess_features(features)
+    
+    recommended_songs = recommend_songs(liked_song_id, features_df, numerical_features)
+    return jsonify({'recommended_songs': recommended_songs})
